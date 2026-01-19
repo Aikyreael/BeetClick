91c63e4fbe79e5c56072109e316cb0a761ae78f6
LILIAN FLIPO <aikyreael@protonmail.com>
2026-01-19 13:41:34 +0100

    init

---
+++ services/bet-service/src/test/java/com/beetclick/betservice/BetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beetclick.betservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
